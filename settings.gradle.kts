ee9f6a0e3dfb3368532ab5a36ca1ec3fa7bb7265
gryteck <gryteck@vk.com>
2023-01-06 16:34:25 +0300

    lab 3 code

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "rsoi-2022-lab3-fault-tolerance-gryteck"
+
+include("common")
+
+include("bonus-service-api")
+include("ticket-service-api")
+include("flight-service-api")
+include("user-service-api")
+
+include("config-server")
+include("gateway")
+include("ticket-service")
+include("user-service")
+include("bonus-service")
+include("flight-service")
