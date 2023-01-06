ba377bd19fdd8d6f0ef64a75c08c1d29a8eea29e
gryteck <gryteck@vk.com>
2023-01-06 17:27:21 +0300

    added k8s deploy

---
+++ k8s/deployTheWorld.sh
@@ -0,0 +1,10 @@
+kubectl apply -f ingress.yml
+kubectl apply -f main-db-configmap.yml
+./deployService.sh "upgrade --install" config-service
+./deployService.sh "upgrade --install" gateway-service
+./deployService.sh "upgrade --install" bonus-service
+./deployService.sh "upgrade --install" flight-service
+./deployService.sh "upgrade --install" ticket-service
+./deployService.sh "upgrade --install" user-service
+
+kubectl get services -o wide
